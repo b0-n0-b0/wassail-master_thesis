0829b7788296b61ebfe955c66d45febd26749969
b0n0b0 <geraci2e@gmail.com>
2025-05-11 15:44:02 +0200

    type

--- lib/analysis/rule_parser/rule_parser.ml
+++ lib/analysis/rule_parser/rule_parser.ml
@@ -1,6 +1,6 @@
 open Core
 open Helpers
-let debug_print (fidx : int) (instr : unit Instr.t) : unit =
+let debug_print (fidx : Int32.t) (instr : unit Instr.t) : unit =
   Printf.printf "Processing instruction in function %d: %s\n"
     fidx (Instr.to_string instr);
   flush stdout
