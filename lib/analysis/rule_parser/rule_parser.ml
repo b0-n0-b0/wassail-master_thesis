bc9e661423062228069270594aa2ea4e7fddb360
b0n0b0 <geraci2e@gmail.com>
2025-05-11 15:32:08 +0200

    fix typo

--- lib/analysis/rule_parser/rule_parser.ml
+++ lib/analysis/rule_parser/rule_parser.ml
@@ -2,5 +2,5 @@ open Core
 open Helpers
 let debug_print (fidx : int32) (instr : unit Instr.t) : unit =
   Printf.printf "Processing instruction in function %d: %s\n"
-    fid (Instr.to_string instr);
+    fidx (Instr.to_string instr);
   flush stdout
